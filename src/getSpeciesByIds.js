c193e13b20b8f45a21c32798775829c8460df199
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-03-21 12:31:12 -0300

    Concluido Rec 1.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // começou
-};
-
+function getSpeciesByIds(...ids) {
+  return species.filter((animal) => ids.find((findAnimal) => findAnimal === animal.id));
+}
+// rest como parâmetro permiter usar n parâmetros na função.
+// a função vai verificar buscando em species pelo parametro informado se existe um id identico ao parâmetro e retornalo.
 module.exports = getSpeciesByIds;
