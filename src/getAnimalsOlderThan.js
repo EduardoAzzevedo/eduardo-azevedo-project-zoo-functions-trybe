c0303feddd428ef334ff16d0e7fb8a6b1c9dee65
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-03-21 13:47:29 -0300

    Concluido Rec 2.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(animalName, animalAge) {
+  // localizar o nome da espécie.
+  // Filter verifica dentro de species se existe algum nome de animal(lions) igual ao passado em seu parâmetro.
+  const filterAnimals = species.filter((specie) => specie.name === animalName);
+  // localiza residente.
+  // Map retorna um array sem modificar o original, no caso, retorna a especie em 'filterAnimals'que foi localizado na função anterior.
+  const mapResidents = filterAnimals.map((resident) => resident.residents);
+  // depois que localizar as informações, every retorna todos os animais da espécie informados tem a idade minima passado como parâmetro, retorna boolean.
+  return mapResidents.every((specie, index) => specie[index].age > animalAge);
 }
-
 module.exports = getAnimalsOlderThan;
