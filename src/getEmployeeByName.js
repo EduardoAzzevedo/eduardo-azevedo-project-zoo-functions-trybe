e74f6d3626a2b198ce9ffc63a659bd25e0b03746
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-03-21 19:33:45 -0300

    Concluido Rec 3.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  // negativa de parâmetro, para retornar de objeto
+  if (!employeeName) return {};
+  const { employees } = data;
+  // tive que reduzir por que é permitido apenas 100 caracteres.
+  // quando passa o primeiro nome 'E' o ultimo nome, abreviado.
+  return employees.find((emp) => emp.firstName === employeeName || emp.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
