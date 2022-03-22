9b8ca3bd309a1a567a650ddde7b3e2f6fe46da61
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-03-22 10:43:46 -0300

    Concluido Rec 5.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  // retornar um objeto com o nome de cada animal como chave, e seu valor é a quantidade que tem daquele animal.
+  if (!animal) {
+    return species.reduce((accumulator, currentValue) => {
+      accumulator[currentValue.name] = currentValue.residents.length;
+      return accumulator;
+    }, {});
+  }
+  // Se informado a especie do animal retorna a quantidade, se também informado o sexo, retorna a quantidade.
+  const animalSpecies = species.find((spec) => spec.name === animal.specie);
+  if (animal.sex) {
+    const animalsSex = animalSpecies.residents.filter((spec) => spec.sex === animal.sex);
+    return animalsSex.length;
+  }
+  return animalSpecies.residents.length;
 }
 
 module.exports = countAnimals;
